22187f0e41dfd15cb24b44f93b1019ffb0233bc0
Denisfx777 <ph254920@gmail.com>
2023-08-24 22:40:52 -0300

    aula01

---
+++ aula01/main.js
@@ -0,0 +1,22 @@
+//console.log
+//consolen eu uso para mostrar uma mensagem ao usuario 
+console.log('Hello');
+
+
+//exibe uma mensagem de alerta
+alert('kk');
+
+
+//para declara variavel no js podemos usar "var", ou "const"
+//sempre que usar "const" para declarar uma variavel o valor nao pode ser alterado 
+const pi = 3.14;
+
+
+alert(pi);
+
+//"let" é uma variavel de escopo local
+//"var" é uma variavel de escopo global
+
+var nome = "denis";
+
+nome = "maria";
